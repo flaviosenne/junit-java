9f39c393a43604cb06d4391c863c7b6e8f532660
joao flavio <flaviosenne123@gmail.com>
2021-08-19 19:29:48 -0300

    first commit

---
+++ src/test/java/com/curso/junit/PrimeiroTeste.java
@@ -0,0 +1,19 @@
+package com.curso.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimeiroTeste {
+
+    @Test
+    public void estruturaDeUmTeste(){
+        // cenario
+        int numero1 =10, numero2 =5;
+
+        // execução
+        int resultado = numero1 + numero2;
+
+        // verificação
+        Assert.assertEquals(15, resultado);
+    }
+}
