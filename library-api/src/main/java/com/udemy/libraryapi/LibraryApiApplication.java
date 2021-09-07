7d80387adbe7bfdf90427736b690a06641a74897
joao flavio <flaviosenne123@gmail.com>
2021-09-07 09:45:56 -0300

    create schedul send email to customes with loans late

--- library-api/src/main/java/com/udemy/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/com/udemy/libraryapi/LibraryApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 @SpringBootApplication
@@ -14,11 +13,6 @@ public class LibraryApiApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
-	}
-
-	@Scheduled(cron = "0 49 21 1/1 * ?")
-	public void testAgendamentoTarefas(){
-		System.out.println("agendamento funcionado");
 	}
 
 	public static void main(String[] args) {
